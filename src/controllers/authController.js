091b66333c070f095c8eaa6fe91fcada05df16fb
Duynguyen21 <nguyenvietduy611@gmail.com>
2020-02-13 06:03:32 +0700

    [1.0.0] Send mail confirm account

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -23,19 +23,36 @@ let postRegister = async (req, res) => {
   }
 
   try {
-   let createdUserSuccess = await auth.register(req.body.email, req.body.gender, req.body.password);
+   let createdUserSuccess = await auth.register(req.body.email, req.body.gender, req.body.password, req.protocol, req.get("host"));
     successAray.push(createdUserSuccess);
     req.flash("success", successAray);
     return res.redirect("/login-register");
+    
   } catch (error) {
     errorArray.push(error);  
     req.flash("errors", errorArray);
     return res.redirect("/login-register");
   }
+};
 
-}
+let verifyAccount = async(req, res) => {
+  let errorArray = [];
+  let successAray = []
+  try {
+    let verifySuccess = await auth.verifyAccount(req.params.token);
+    successAray.push(verifySuccess);
+    req.flash("success", successAray);
+    return res.redirect("/login-register");
+
+  } catch (error) {
+    errorArray.push(error);
+    req.flash("error", errorArray);
+    return res.redirect("/login-register");
+  }
+};
 
 module.exports = {
   getLoginRegister : getLoginRegister,
-  postRegister: postRegister
+  postRegister: postRegister,
+  verifyAccount: verifyAccount
 };
--- src/services/authService.js
+++ src/services/authService.js
@@ -1,42 +1,70 @@
 import UserModel from "./../models/userModel";
 import bcrypt from "bcrypt";
 import uuidv4 from "uuid/v4";
-import {transErrors, transSuccess} from "./../../lang/vi";
+import {
+  transErrors,
+  transSuccess,
+  transMail
+} from "./../../lang/vi";
+import sendMail from "./../config/emailler";
 
 let saltRounds = 7;
 
-let register = (email, gender, password) => {
+let register = (email, gender, password, protocol, host) => {
   return new Promise(async (resolve, reject) => {
     let userByEmail = await UserModel.findByEmail(email);
-  if(userByEmail){
-    if(userByEmail.deletedAt() != null){
-      return reject(transErrors.account_removed);
+    if (userByEmail) {
+      if (userByEmail.deletedAt() != null) {
+        return reject(transErrors.account_removed);
 
-    }
-    if(!userByEmail.local.isActive){
-      return reject(transErrors.account_not_active);
-    }
-    return reject(transErrors.account_in_use);
-  };
+      }
+      if (!userByEmail.local.isActive) {
+        return reject(transErrors.account_not_active);
+      }
+      return reject(transErrors.account_in_use);
+    };
 
-  let salt = bcrypt.genSaltSync(saltRounds);
-  let userItem = {
-    userName: email.split("@")[0],
-    gender: gender,
-    local: {
-      email: email,
-      password: bcrypt.hashSync(password, salt),
-      veryfileToken : uuidv4()
-    }
-  };
+    let salt = bcrypt.genSaltSync(saltRounds);
+    let userItem = {
+      userName: email.split("@")[0],
+      gender: gender,
+      local: {
+        email: email,
+        password: bcrypt.hashSync(password, salt),
+        verifyToken: uuidv4()
+      }
+    };
 
-  let user = await UserModel.createNew(userItem);
-  resolve(transSuccess.userCreated(user.local.email));
-
+    let user = await UserModel.createNew(userItem);
+    let linkVerify = `${protocol}://${host}/verify/${user.local.verifyToken}`;
+    // send email
+    sendMail(email, transMail.subject, transMail.template(linkVerify))
+      .then(success => {
+        resolve(transSuccess.userCreated(user.local.email));
+        console.log(linkVerify)
+      })
+      .catch(async (error) => {
+        // remove user
+        await UserModel.removeById(user._id);
+        console.log(error);
+        reject(transMail.send_failed);
+      });
   });
-  
+};
+
+let verifyAccount = (token) => {
+  return new Promise(async (resolve, reject) => {
+    let userByToken = await UserModel.findByToken(token);
+    if (!userByToken) {
+      return reject(transErrors.token_undefined);
+    }
+    await UserModel.verify(token);
+    resolve(transSuccess.account_active);
+  });
+
 };
 
 module.exports = {
-  register: register
+  register: register,
+  verifyAccount: verifyAccount
 }
