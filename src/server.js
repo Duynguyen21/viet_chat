13b52ed403cfe654b98693f0e8f7f3644e460d74
Duynguyen21 <nguyenvietduy611@gmail.com>
2020-02-12 10:45:40 +0700

    [1.0.0] Add routers and controllers

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import ConnectDB from "./config/connectDB";
 import configViewEngine from "./config/viewEngine";
+import initRouters from "./routers/web";
 
 // init app
 let app = express();
@@ -11,14 +12,8 @@ ConnectDB();
 // Config viewEngine
 configViewEngine(app);
 
-app.get("/", (req, res) => {
-    return res.render("main/master");
-
-});
-
-app.get("/login-register", (req, res) => {
-    return res.render("auth/loginRegister");
-});
+// Init all  routers
+initRouters(app);
 
 app.listen(process.env.APP_PORT, process.env.APP_HOST, () =>{
     console.log(`Hello Duy Nguyen,Im running at: ${process.env.APP_HOST}:${process.env.APP_PORT}/`)
